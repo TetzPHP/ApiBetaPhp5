d561b4cd30bcfc527b2acda9a16eef79d5d0e234
TetzPHP <TetzPHP@users.noreply.github.com>
2014-03-16 21:00:31 +0100

    Mise à jour de Serie pour prendre en compte "seasons_details"

--- Apibeta/Objet/Serie.php
+++ Apibeta/Objet/Serie.php
@@ -6,8 +6,7 @@ namespace Apibeta\Objet;
  * Représente une série
  * @author TetzPHP
  */
-class Serie extends Generique
-{
+class Serie extends Generique {
 
     protected $id;
     protected $thetvdb_id;
@@ -15,6 +14,7 @@ class Serie extends Generique
     protected $title;
     protected $description;
     protected $seasons;
+    protected $seasons_details;
     protected $episodes;
     protected $followers;
     protected $comments;
@@ -37,121 +37,98 @@ class Serie extends Generique
      * 
      * @return Episode[]
      */
-    public function getUnseen()
-    {
+    public function getUnseen() {
         return $this->unseen;
     }
 
-    protected function setUnseen($unseen)
-    {
+    protected function setUnseen($unseen) {
         $this->unseen = Episode::tableau($unseen);
     }
 
-    public function getId()
-    {
+    public function getId() {
         return $this->id;
     }
 
-    public function getThetvdb_id()
-    {
+    public function getThetvdb_id() {
         return $this->thetvdb_id;
     }
 
-    public function getImdb_id()
-    {
+    public function getImdb_id() {
         return $this->imdb_id;
     }
 
-    public function getTitle()
-    {
+    public function getTitle() {
         return $this->title;
     }
 
-    public function getDescription()
-    {
+    public function getDescription() {
         return $this->description;
     }
 
-    public function getSeasons()
-    {
+    public function getSeasons() {
         return $this->seasons;
     }
 
-    public function getEpisodes()
-    {
+    public function getEpisodes() {
         return $this->episodes;
     }
 
-    public function getFollowers()
-    {
+    public function getFollowers() {
         return $this->followers;
     }
 
-    public function getComments()
-    {
+    public function getComments() {
         return $this->comments;
     }
 
-    public function getSimilars()
-    {
+    public function getSimilars() {
         return $this->similars;
     }
 
-    public function getCharacters()
-    {
+    public function getCharacters() {
         return $this->characters;
     }
 
-    public function getCreation()
-    {
+    public function getCreation() {
         return $this->creation;
     }
 
-    public function getGenres()
-    {
+    public function getGenres() {
         return $this->genres;
     }
 
-    public function getLength()
-    {
+    public function getLength() {
         return $this->length;
     }
 
-    public function getNetwork()
-    {
+    public function getNetwork() {
         return $this->network;
     }
 
-    public function getRating()
-    {
+    public function getRating() {
         return $this->rating;
     }
 
-    public function getStatus()
-    {
+    public function getStatus() {
         return $this->status;
     }
 
-    public function getLanguage()
-    {
+    public function getLanguage() {
         return $this->language;
     }
 
     /**
      * @return Note
      */
-    public function getNotes()
-    {
+    public function getNotes() {
         return $this->notes;
     }
 
-    public function getIn_account()
-    {
+    public function getIn_account() {
         return $this->in_account;
     }
 
-    public function getUser()
-    {
+    public function getUser() {
         if ($this->in_account) {
             return $this->user;
         } else {
@@ -159,118 +136,104 @@ class Serie extends Generique
         }
     }
 
-    public function getResource_url()
-    {
+    public function getResource_url() {
         return $this->resource_url;
     }
 
-    protected function setId($id)
-    {
+    public function getSeasons_details() {
+        return $this->seasons_details;
+    }
+
+    protected function setSeasons_details($seasons_details) {
+        $this->seasons_details = $seasons_details;
+    }
+
+    protected function setId($id) {
         $this->id = $id;
     }
 
-    protected function setThetvdb_id($thetvdb_id)
-    {
+    protected function setThetvdb_id($thetvdb_id) {
         $this->thetvdb_id = $thetvdb_id;
     }
 
-    protected function setImdb_id($imdb_id)
-    {
+    protected function setImdb_id($imdb_id) {
         $this->imdb_id = $imdb_id;
     }
 
-    protected function setTitle($title)
-    {
+    protected function setTitle($title) {
         $this->title = $title;
     }
 
-    protected function setDescription($description)
-    {
+    protected function setDescription($description) {
         $this->description = $description;
     }
 
-    protected function setSeasons($seasons)
-    {
+    protected function setSeasons($seasons) {
         $this->seasons = $seasons;
     }
 
-    protected function setEpisodes($episodes)
-    {
+    protected function setEpisodes($episodes) {
         $this->episodes = $episodes;
     }
 
-    protected function setFollowers($followers)
-    {
+    protected function setFollowers($followers) {
         $this->followers = $followers;
     }
 
-    protected function setComments($comments)
-    {
+    protected function setComments($comments) {
         $this->comments = $comments;
     }
 
-    protected function setSimilars($similars)
-    {
+    protected function setSimilars($similars) {
         $this->similars = $similars;
     }
 
-    protected function setCharacters($characters)
-    {
+    protected function setCharacters($characters) {
         $this->characters = $characters;
     }
 
-    protected function setCreation($creation)
-    {
+    protected function setCreation($creation) {
         $this->creation = $creation;
     }
 
-    protected function setGenres($genres)
-    {
+    protected function setGenres($genres) {
         $this->genres = $genres;
     }
 
-    protected function setLength($length)
-    {
+    protected function setLength($length) {
         $this->length = $length;
     }
 
-    protected function setNetwork($network)
-    {
+    protected function setNetwork($network) {
         $this->network = $network;
     }
 
-    protected function setRating($rating)
-    {
+    protected function setRating($rating) {
         $this->rating = $rating;
     }
 
-    protected function setStatus($status)
-    {
+    protected function setStatus($status) {
         $this->status = $status;
     }
 
-    protected function setLanguage($language)
-    {
+    protected function setLanguage($language) {
         $this->language = $language;
     }
 
-    protected function setNotes($notes)
-    {
+    protected function setNotes($notes) {
         $this->notes = new Note($notes);
     }
 
-    protected function setIn_account($in_account)
-    {
+    protected function setIn_account($in_account) {
         $this->in_account = $in_account;
     }
 
-    protected function setUser($user)
-    {
+    protected function setUser($user) {
         $this->user = $user;
     }
 
-    protected function setResource_url($resource_url)
-    {
+    protected function setResource_url($resource_url) {
         $this->resource_url = $resource_url;
     }
+
 }
